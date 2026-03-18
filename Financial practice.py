cda7861127149fec1faee84268ec3cedc747a9ed
Acedigitals <davidodii695@gmail.com>
2026-03-18 04:02:46 +0100

    Modified the Portolio class
    Added some extra features to the add_postion funtion
    This features includes enabling the function to get the price from a price list
    Created a get_instrument_Price function which helps to fetch the price of an instrument from a dictionary
    Finally this two funtions leads to the calc_portfolio value funtion which returns the current value of the portfolio based on the investors holdings.
    
    next stop: Refining the classes, then calculation the weights and risks

--- Financial practice.py
+++ Financial practice.py
@@ -1,30 +1,78 @@
+import numpy as np
+
 class InvestorProfile:
-    def __init__(self, holdings, instruments, risks, speculations):
+    def __init__(self, holdings:int|list, instruments: str|list, risks, speculations, investment_price_list: dict):
         self.holdings = holdings
-        self.instruments = instruments
+        self.instruments = instruments # This one is for all the currently available investments 
         self.risks = risks
         self.speculations = speculations
         self.returns = []
+        self.investment_price_list = investment_price_list # this is is just a price list. and it is updated with new postions that are added 
 class Portfolio(InvestorProfile):
-    def __init__(self, holdings, instruments, risks, speculations):
-        super().__init__(holdings, instruments, risks, speculations)
+    def __init__(self, holdings, instruments, risks, speculations, investment_price_list):
+        super().__init__(holdings, instruments, risks, speculations, investment_price_list)
         self.value = 0
-        self.investmets =  list() # name of assest, and values of invested amounts "Apple": 50usd
-        self.positions = dict() # namme of assets and values of holdings, "Apple": 20 units
+        self.investmets =  list() # name of assest, and values of invested amounts "Apple": 50usd, the investements are holdings * price, sum(holdings * price)
+        self.positions = dict() # namme of all open postions A
         self.weight = None
         self.price = None
-    def print_retutrns(self):
-        return self.returns
+
+    def get_instrument_price(self, asset_name: str|list):
+        if type(asset_name) == str:
+            if asset_name in self.investment_price_list:
+                self.price = self.investment_price_list[asset_name]
+                return self.price
+            else:
+                print("This instrument is not on your watchlist, recheck name or add the instrument")
+                asset_name = input(f"""
+                                   Re-enter name of the instrument, 
+                                   Available Instrumnets include 
+                                   {list(self.investment_price_list.keys())}:
+
+                                   >> """)
+                if asset_name in self.investment_price_list:
+                    self.price = self.investment_price_list[asset_name]
+                    return self.price
+                else:
+                    self.get_instrument_price(asset_name)
+                    return self.price
+        elif type(asset_name) == list:
+            asset_prices = []
+            for name in asset_name:
+                if name in self.investment_price_list:
+                    asset_prices.append(self.investment_price_list[name])
+                else:
+                    print(f"{name} instrument is not on your watchlist, recheck name or add the instrument")
+                    print(f"""
+                    Available Instrumnets include 
+                    {list(self.investment_price_list.keys())}
+""")
+                    asset_prices.append(None)
+                    continue
+            self.price = asset_prices
+            return self.price
     def calc_portfolio_value(self):
-        return self.value
-    def add_position(self, name:str|list, holdings:float|int|list): #, name:str|list, holdings:float|int
+        price  = np.array(self.get_instrument_price(self.instruments))
+        holdings =  np.array(self.holdings)
+        value = np.sum(price * holdings)
+        return value
+        # pass
+        # initial_value = np.array(self.holdings) * np.array(instruments_price)
+        
+    def add_position(self, name:str|list, holdings:float|int|list, price:int): #, name:str|list, holdings:float|int
         if (type(name) == list) & (type(holdings) == list):
             for i,value in enumerate(name):
                 self.positions[value]= holdings[i]
+                self.investment_price_list[value] = price
+                self.holdings.append[holdings[i]]
             return self.positions
         else:
+            self.investment_price_list[value] = price
             self.positions.update({
                 name:holdings
+            })
+            self.investment_price_list.update({
+                name:price
             })
             return self.positions
     def calc_returns():
@@ -33,16 +81,28 @@ class Portfolio(InvestorProfile):
         return f"This is the Portfolio class in progress"
     
 # portfolio.calc_portfolio_value()
-# print(portfolio.add_position(name = ["Apple", "BTC"],holdings= [70, 50]))
-
+# print(portfolio.add_position(name = ["Apple", "BTC"],holdings= [70, 50]))        
 holdings = [20, 45, 35, 10, 12]
 instruments = ["Apple", "Microsoft", "Oil", "Gold", "Bitcoin"]
-risks = [0.1, 0.2, 0.3, 0.4, 0.5]
+risks = [0.1, 0.2, 0.3] 
 speculations = ["Tesla", "Nvidia", "Sugar", "Cattle", "Silver"]
-Investor = InvestorProfile(holdings, instruments, risks, speculations)
-InvestorPortfolio = Portfolio(Investor.holdings,Investor.instruments, Investor.risks, Investor.speculations)
 
-print(InvestorPortfolio.print_retutrns())
+investment_price_list = {
+    "Bitcoin": 70000,
+    "Gold": 5100,
+    "Apple": 300, 
+    "Microsoft": 291, 
+    "Oil": 50,
+    "Tesla": 321, 
+    "Nvidia":435, 
+    "Sugar":20, 
+    "Cattle":400, 
+    "Silver":75
+}
 
-Investor2 = InvestorProfile(holdings=15, instruments="Dangote Group", risks=0.1, speculations="Samsung")
-print(Investor2.holdings)
+Investor = InvestorProfile(holdings, instruments, risks, speculations, investment_price_list)
+InvestorPortfolio = Portfolio(Investor.holdings,Investor.instruments, Investor.risks, Investor.speculations, Investor.investment_price_list)
+# print(InvestorPortfolio.add_position("Solana", 40, 120))
+print(InvestorPortfolio.positions)
+print(InvestorPortfolio.calc_portfolio_value())
+# print(InvestorPortfolio.__dict__)
